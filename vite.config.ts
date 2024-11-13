9b98b3c08d2133ec7d5ba83260fce05c5b341f16
andreisoroka <unickpro@gmail.com>
2024-11-14 00:02:19 +0200

    Switch scss to modern-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,11 @@ export default defineConfig({
     // https://forum.babylonjs.com/t/unable-to-load-havok-plugin-error-while-loading-wasm-file-from-browser/40289/28?page=2
     exclude: ['@babylonjs/havok'],
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
 });
